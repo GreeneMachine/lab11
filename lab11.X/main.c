36e3cddd4bd6b91c218f31a84abd21c5955334a6
ongsh <ongsh@192.168.0.13>
2020-04-07 20:23:44 -0500

    shuee's commit

--- lab11.X/main.c
+++ lab11.X/main.c
@@ -17,10 +17,6 @@
 // assignment, course failure and a report to the Academic Dishonesty
 // Board.
 //*****************************************************************
-
-
-this is a test uno dos tres
-
 #include "mcc_generated_files/mcc.h"
 #pragma warning disable 520     
 #pragma warning disable 1498
